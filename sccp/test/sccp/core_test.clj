5646d26fa97af21427c01fc7d4a00b7285a0b032
David Hanley <ctchrinthry@gmail.com>
2014-04-23 14:36:42 -0500

    more tests

--- sccp/test/sccp/core_test.clj
+++ sccp/test/sccp/core_test.clj
@@ -4,10 +4,17 @@
 
 (deftest basic-square-test
   (testing "basic square stuff"
-    (is (= (square-to-string 45) "f3"))
-    (is (axis-ok 1))
-    (is (not (axis-ok -1)))
-    (is (axis-ok 7))
-    (is (not (axis-ok 8)))
-    )
-)
+	   (is (= (square-to-string 45) "f3"))
+	   (is (axis-ok 1))
+	   (is (not (axis-ok -1)))
+	   (is (axis-ok 7))
+	   (is (not (axis-ok 8)))
+	   )
+  )
+
+(deftest simple-piece
+  (testing "basic piece move"
+	   (is (= [[1 0] [0 1] [1 1]] (hopper-to [0 0] king-deltas)))
+	   (is (= [[1 2] [2 1]] (hopper-to [0 0] knight-deltas)))
+	   )   
+  )   
